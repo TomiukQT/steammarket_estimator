0e34a1870c14aaa9a33b3353e823a9723f754a1c
Tomáš Valenta <TomiukQT@gmail.com>
2022-10-28 12:43:10 +0200

    feature: Basic price prediction

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import steammarket as sm
 import prices_parser as parser
+import price_estimator
 
 CSGO_ID = 730
 TEST_ITEM = 'AK-47 | Redline (Field-Tested)'
-
+TEST_ITEM2 = 'Operation Breakout Weapon Case'
 
 def main():
     item_info = sm.get_item(CSGO_ID, TEST_ITEM)
     print(item_info)
-    item_history = sm.get_item_history(CSGO_ID, TEST_ITEM)
+    item_history = sm.get_item_history(CSGO_ID, TEST_ITEM2)
     df = parser.parse_item_history(item_history)
     df = df.astype({
         'date': 'datetime64',
         'amount': 'int64',
     })
-    print(df.info())
+    print(df.head(2))
+    price_estimator.process(df)
 
 
 if __name__ == '__main__':
